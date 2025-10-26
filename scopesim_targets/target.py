9326ad44bfa3bd939e8675abd95e6f1aa6034a90
teutoburg <ghost@instruct.at>
2025-10-26 23:45:26 +0100

    Make `Target` abstract

--- scopesim_targets/target.py
+++ scopesim_targets/target.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 """Contains main ``Target`` class."""
 
+from abc import ABCMeta, abstractmethod
 
-class Target:
+
+class Target(metaclass=ABCMeta):
     """Main class in scopesim-targets."""
 
+    @abstractmethod
     def to_source(self):
         """Convert to ScopeSim Source object."""
         raise NotImplementedError()
