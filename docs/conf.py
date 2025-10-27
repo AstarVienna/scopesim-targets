8764e6034942228374a749bc3127967081583260
teutoburg <ghost@instruct.at>
2025-10-27 13:46:16 +0100

    Add more sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -1,2 +1,12 @@
 # -*- coding: utf-8 -*-
 """Sphinx configuration."""
+
+source_suffix = [".rst", ".md"]
+
+html_theme = "sphinx_book_theme"
+html_theme_options = {
+    "repository_url": "https://github.com/AstarVienna/scopesim-targets",
+    "use_repository_button": True,
+    "use_download_button": True,
+    "home_page_in_toc": True,
+}
