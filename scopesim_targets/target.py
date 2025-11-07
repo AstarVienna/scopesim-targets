ef0f59bfb80f4618e5552897bf1fc024ac3e595d
teutoburg <ghost@instruct.at>
2025-11-07 22:09:41 +0100

    Add basic position parser

--- scopesim_targets/target.py
+++ scopesim_targets/target.py
@@ -3,6 +3,9 @@
 
 from abc import ABCMeta, abstractmethod
 
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
 
 class Target(metaclass=ABCMeta):
     """Main class in scopesim-targets."""
@@ -17,6 +20,15 @@ class Target(metaclass=ABCMeta):
         """Target position (center) as SkyCoord."""
         return self._position
 
+    # TODO: add typing
     @position.setter
     def position(self, position):
-        self._position = position
+        match position:
+            case SkyCoord():
+                self._position = position
+            case (x_arcsec, y_arcsec) | {"x": x_arcsec, "y": y_arcsec}:
+                x_arcsec <<= u.arcsec
+                y_arcsec <<= u.arcsec
+                self._position = SkyCoord(x_arcsec, y_arcsec)
+            case _:
+                raise TypeError("Unkown postition format.")
