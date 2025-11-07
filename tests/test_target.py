7de3f93459a55501d55a654ade92febe0bdd05eb
teutoburg <ghost@instruct.at>
2025-11-07 22:08:24 +0100

    Fix test for abstract base class

--- tests/test_target.py
+++ tests/test_target.py
@@ -6,5 +6,11 @@ from scopesim_targets.target import Target
 
 class TestTarget:
     def test_basic(self):
-        tgt = Target()
+        # Note: This is necessary because Target is an abstract base class which
+        #       cannot be instantiated directly. The hack below allows to run a
+        #       super basic test on it, without using any of its actual subcls.
+        class MockTargetSubcls(Target):
+            def to_source(self):
+                pass
+        tgt = MockTargetSubcls()
         assert isinstance(tgt, Target)
