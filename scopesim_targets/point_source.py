1e2c705ba6eca1b3d1f761343188ec316803aa71
teutoburg <ghost@instruct.at>
2025-11-09 22:04:15 +0100

    Implement `PointSourceTarget` base class

---
+++ scopesim_targets/point_source.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""Currently only ``Star`` and baseclass."""
+
+from astropy import units as u
+
+from .target import SpectrumTarget
+
+
+class PointSourceTarget(SpectrumTarget):
+    """Base class for Point Source Targets."""
+
+    def _xy_arcsec_position(self, local_frame) -> tuple[float, float]:
+        # Transform to local offset for ScopeSim
+        local_position = self.position.transform_to(local_frame)
+
+        # ra, dec turn into lon, lat in offset frame, .round(6) is microarcsec
+        x_arcsec = local_position.lon.to_value(u.arcsec).round(6)
+        y_arcsec = local_position.lat.to_value(u.arcsec).round(6)
+        return x_arcsec, y_arcsec
+
+    def _to_table_row(
+        self,
+        local_frame=None,
+        spectrum=None,
+        ref: int = 0,
+    ) -> dict[str, float]:
+        # If not given from parent, position is always (0, 0) locally
+        if local_frame is None:
+            local_frame = self.position.skyoffset_frame()
+        x_arcsec, y_arcsec = self._xy_arcsec_position(local_frame)
+
+        # If not given from parent, resolve now
+        if spectrum is None:
+            spectrum = self.resolve_spectrum()
+        weight = self._get_spectrum_scale(spectrum)
+
+        row = {
+            "x": x_arcsec,
+            "y": y_arcsec,
+            "weight": weight,
+            "ref": ref,
+        }
+        return row
