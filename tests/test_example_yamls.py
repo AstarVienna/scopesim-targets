00bb9b3ec0749616d1d7810dd6ba3125c45974e6
teutoburg <ghost@instruct.at>
2025-11-19 12:39:56 +0100

    Make sure example yaml test work and allow debug prints

--- tests/test_example_yamls.py
+++ tests/test_example_yamls.py
@@ -8,6 +8,10 @@ import yaml
 
 from scopesim_targets.target import Target
 
+# HACK: Temporary to ensue proper import and yaml tag registring
+from scopesim_targets.point_source import *
+from scopesim_targets.extended_source import *
+
 
 EXAMPLE_YAML_PATH = Path(__package__).parent / "docs"
 
@@ -15,6 +19,7 @@ EXAMPLE_YAML_PATH = Path(__package__).parent / "docs"
 @pytest.mark.xfail
 @pytest.mark.parametrize("file", EXAMPLE_YAML_PATH.rglob("*.yaml"))
 def test_examle_yamls_parse(file):
+    print(file.name)
     with file.open("r", encoding="utf-8") as stream:
         tgt = yaml.full_load(stream)
     assert isinstance(tgt, Target)
@@ -23,6 +28,7 @@ def test_examle_yamls_parse(file):
 @pytest.mark.xfail
 @pytest.mark.parametrize("file", EXAMPLE_YAML_PATH.rglob("*.yaml"))
 def test_examle_yamls_can_make_source(file):
+    print(file.name)
     with file.open("r", encoding="utf-8") as stream:
         tgt = yaml.full_load(stream)
     tgt.to_source()
