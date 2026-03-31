aa21e63a6895c633eefe4fe8d45131d8cd28916d
teutoburg <ghost@instruct.at>
2026-03-31 15:31:21 +0200

    Refactor default IMFs

--- scopesim_targets/stellar/imf.py
+++ scopesim_targets/stellar/imf.py
@@ -37,15 +37,35 @@ class LogNormal(rv_continuous):
         return x**-delta * np.exp(-(mo / x)**beta) * 15
 
 
-# Kroupa 2002
-# 2002Sci...295...82K
-params = {
-    "a0": .3, "a1": 1.3, "a2": 2.3, "a3": 2.7,
-    "m0": .01, "m1": .08, "m2": .5, "m3": 1.,
-}
+# FIXME: These get auto-generated docstrings from the base class, so they should
+#        show up on RTD, but don't. See "attributes" in the module template...
+# TODO: Consider renaming these to something more generic if the PDFs are used
+#       by other laws as well. Find out more about upper and lower bounds
+#       of each IMF before that!! Can those ("support") be set in the "freezing"
+#       step afterwards??
 kroupa_gen = BrokenPowerlaw(name="Kroupa02", a=0.005, b=150, momtype=0)
-kroupa = kroupa_gen(**params)
-
-# Chabrier 2001
 chabrier_gen = LogNormal(name="Chabrier01", a=0.005, b=150)
-chabrier = chabrier_gen(mo=716.4, delta=3.3, beta=.25)
+
+
+def load_default_imfs() -> dict[str, rv_continuous]:
+    """Return "frozen" IMF distributions with default values."""
+    imfs = {}
+
+    # Kroupa 2002
+    # 2002Sci...295...82K
+    kroupa_02_params = {
+        "a0": .3, "a1": 1.3, "a2": 2.3, "a3": 2.7,
+        "m0": .01, "m1": .08, "m2": .5, "m3": 1.,
+    }
+    imfs["kroupa02"] = kroupa_gen(**kroupa_02_params)
+
+    # Chabrier 2001
+    chabrier_01_params = {
+        "mo": 716.4, "delta": 3.3, "beta": .25,
+    }
+    imfs["chabrier01"] = chabrier_gen(**chabrier_01_params)
+
+    return imfs
+
+
+DEFAULT_IMFS = load_default_imfs()
