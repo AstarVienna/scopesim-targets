43d4772eb4ce7aecc91a2bc4d075e32c070b55c2
teutoburg <ghost@instruct.at>
2026-04-10 12:23:20 +0200

    Implement mass to brightness for stellar population

--- scopesim_targets/stellar/populations.py
+++ scopesim_targets/stellar/populations.py
@@ -15,6 +15,8 @@ class Population:
 
     def __init__(self, n_stars: int):
         self._n_stars = n_stars
+        # TODO: Consider using a singelton-ish thing here
+        self._stellar_params = StellarParameters()  # Default lookup table
 
 
 class ZeroAgePopulation(Population):
@@ -43,7 +45,19 @@ class IMFPopulation(ZeroAgePopulation):
         n_stars = int(total_mass.to_value(u.solMass) / imf.expect())
         return cls(n_stars, imf)
 
-    def sample_imf(self) -> np.ndarray:
+    def sample_imf(self) -> u.Quantity[u.solMass]:
         urng = np.random.default_rng()
         rng = NumericalInversePolynomial(self.imf, center=0.1, random_state=urng)
-        return rng.rvs(self._n_stars)
+        return rng.rvs(self._n_stars).round(3) * u.solMass
+
+    def _masses_to_brightness(self, masses, absmag_col: str):
+        absmags = (
+            self._stellar_params
+            .interpolate("mass", masses, extrapolate_phot=True)[absmag_col]
+            .round(2)
+        )
+        if hasattr(absmags, "mask") and not absmags.mask.any():
+            # ditch any remaining empty masks
+            return absmags.unmasked
+        return absmags
+
