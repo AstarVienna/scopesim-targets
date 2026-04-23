0e7ae58793f0157643392704c7b84ef00ada82b0
teutoburg <ghost@instruct.at>
2026-04-23 19:22:45 +0200

    Add `ZeroAgeCluster` target class
    For now not a subclass of `Target`, but implements a similar protocol.

---
+++ scopesim_targets/cluster.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""TBA."""
+
+from astropy import units as u
+from astropy.table import Table
+
+from scopesim import Source
+from scopesim.source.source_fields import TableSourceField
+
+from .typing_utils import POSITION_TYPE
+from .stellar.populations import Population, ZeroAgePopulation
+from .stellar.morphology import Morphology
+
+
+class Cluster:
+    def __init__(
+        self,
+        position: POSITION_TYPE,
+        population: Population,
+        morphology: Morphology,
+    ) -> None:
+        self.position = position
+        self.population = population
+        self.morphology = morphology
+
+
+class ZeroAgeCluster(Cluster):
+    def __init__(
+        self,
+        position: POSITION_TYPE,
+        population: ZeroAgePopulation,
+        morphology: Morphology,
+    ) -> None:
+        super().__init__(position, population, morphology)
+
+    def to_source(self):
+        src_coldict, spectra = self.population.to_source_columns(self.position)
+        src_coldict.update(self.morphology.to_source_columns(self.position))
+
+        tbl = Table(
+            data=src_coldict,
+            names=["x", "y", "ref", "weight"],
+            units={"x": u.arcsec, "y": u.arcsec},
+        )
+        src = Source(field=TableSourceField(tbl, spectra=spectra))
+        return src
+
+# StarField.from_??(Population, Morphology) ???
+
+# but where to put parameters such that variable??
